46ac8f11c03e15f4818a17f32684d64a3940a820
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2025-01-01 14:38:22 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+//getElementsByTagName()
+// let a = document.getElementsByTagName('ul')
+// console.log(a);
+
+//getElementsByClassName()
+// let b = document.getElementsByClassName("hero")
+// console.log(b);
+
+
+//getElementById()
+// let c = document.getElementById("manes")
+// console.log(c);
+
+
+//querySelector() //only first property show. It's like includes method.
+// let d = document.querySelector(".hero")
+// console.log(d);
+
+
+//querySelectorAll()
+// let e = document.querySelectorAll("li")
+// console.log(e);
+
+
+
+
+//------Traverse The DOM------
+
+//parentElement;
+// let parent = document.querySelector('ul')
+// let result = parent.parentElement;
+// console.log(result);
+
+
+//children
+// let son = document.querySelector(".top")
+// console.log(son.children[0]);
+
+//childNodes
+// let sun = document.querySelector(".top")
+// console.log(sun.childNodes[1])
+
+//PreviousElementSibling
+
+//appendChild
+
+//remove
+
+//replaceChild
+
+//parentNode
+
+//previousSibling
+
+//firstChild
+
+//lastChild
